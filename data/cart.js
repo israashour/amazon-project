24226795babb3ab140a49129949d70006b3fcb8b
ashourisra <85707708+israashour@users.noreply.github.com>
2025-02-04 01:35:41 +0200

    Save cart into localStorage

--- data/cart.js
+++ data/cart.js
@@ -1,13 +1,22 @@
-export let cart = [{
-  productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-  quantity: 2
-},
-{
-  productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-  quantity: 1
-},];
+export let cart = JSON.parse(localStorage.getItem('cart'));
 
- export function addToCart(productId) {
+if (!cart) {
+  cart = [{
+    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity: 2
+  },
+  {
+    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity: 1
+  },];
+};
+
+
+function saveToStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
+
+export function addToCart(productId) {
   let matchingItem;
 
   cart.forEach((cartItem) => {
@@ -24,6 +33,7 @@ export let cart = [{
       quantity: 1,
     });
   }
+  saveToStorage();
 }
 
 export function updateCartQuantity() {
@@ -46,4 +56,6 @@ export function removeFromCart(productId) {
   });
 
   cart = newCart;
+
+  saveToStorage();
 }

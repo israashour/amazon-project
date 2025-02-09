1d1e339f25be40a117d5970fc9dbfcd61632e01c
ashourisra <85707708+israashour@users.noreply.github.com>
2025-02-09 23:44:12 +0200

    Make search-bar & search-button interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -8,7 +8,17 @@ function renderProductsGrid() {
 
   let productsHTML = '';
 
-  products.forEach((product) => {
+  const url = new URL(window.location.href);
+  const search = url.searchParams.get('search');
+
+  let filteredProducts = products;
+
+  if (search) {
+    filteredProducts = products.filter((product) => {
+      return product.name.includes(search);
+    });
+  }
+  filteredProducts.forEach((product) => {
     productsHTML += `
   <div class="product-container">
           <div class="product-image-container">
@@ -74,4 +84,10 @@ function renderProductsGrid() {
 
     })
   });
+
+  document.querySelector('.js-search-button')
+    .addEventListener('click', () => {
+      const search = document.querySelector('.js-search-bar').value;
+      window.location.href = `amazon.html?search=${search}`;
+    });
 };
